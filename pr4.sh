fc819bb453d9b5a6397d2cceba40e8e6421178a3
balaji38738 <39882654+balaji38738@users.noreply.github.com>
2020-03-11 11:07:55 +0530

    Update pr4.sh

--- pr4.sh
+++ pr4.sh
@@ -1,5 +1,8 @@
 #! /bin/bash -x
 
+#Problem Statement :- Generating 5 two digit numbers.Finding sum and average of them
+#Author :- Balaji Ijjapwar
+#Date :- 11 March 2020
 sum=$(( RANDOM % 90 + 10 ))  #generates numbers between 10 to 90
 sum=$(( RANDOM % 90 + 10 + sum ))
 sum=$(( RANDOM % 90 + 10 + sum ))
