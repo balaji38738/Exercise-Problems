6a0602335d6444ae84862f263ff190d8c2441689
balaji38738 <39882654+balaji38738@users.noreply.github.com>
2020-03-11 11:13:18 +0530

    Update pr8.sh

--- pr8.sh
+++ pr8.sh
@@ -1,5 +1,9 @@
 #! /bin/bash -x
 
+#Problem Statement :- Arithmetic operations on three numbers
+#Author :- Balaji Ijjapwar
+#Date :- 11 March 2020
+
 read -p "Enter 3 numbers: " number1 number2 number3
 result=$(( $number1 + $number2 * $number3 ))
 echo result
