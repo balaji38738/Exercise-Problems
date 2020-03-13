5e7cee63e326ec54679f8a0150215fe1c61a795e
balaji38738 <39882654+balaji38738@users.noreply.github.com>
2020-03-13 19:07:46 +0530

    Update PrimeFactors.sh

--- PrimeFactors.sh
+++ PrimeFactors.sh
@@ -6,9 +6,7 @@
 
 read -p "Enter number: " number
 
-lastNumber=$(( number / 2 + 1 )) #Maximum possible prime factor
-
-for (( num=2; num<=$lastNumber; num++ ))
+for (( num=2; num<=$number; num++ ))
 do
 	for (( ; $(( number % num ))==0; ))
 	do
