1773d42ac7ef6842b385b8c07884ae453850baae
balaji38738 <bijjapwar123@gmail.com>
2020-03-13 15:47:30 +0530

    Updated UniConversion2.sh

--- UnitConversion2.sh
+++ UnitConversion2.sh
@@ -4,18 +4,33 @@
 #Author:- Balaji Ijjapwar
 #Date:- 12 March 2020
 
-read -p "Enter feet: " feet
-inch=`echo "scale=4; $feet * 12" | bc`
-echo "$feet feet = $inch inch"
+echo "Enter 1 for feet to inch"
+echo "Enter 2 for inch to feet"
+echo "Enter 3 for feet to meter"
+echo "Enter 4 for meter to feet"
 
-read -p "Enter inch: " inch
-feet=`echo "scale=4; $inch / 12" | bc`
-echo "$inch inch = $feet feet"
+read choice
 
-read -p "Enter feet: " feet
-meter=`echo "scale=4; $feet * 0.3048" | bc`
-echo "$feet feet = $meter meter"
+feetToInch=1
+inchToFeet=2
+feetToMeter=3
+meterToFeet=4
 
-read -p "Enter meter: " meter
-feet=`echo "scale=4; $meter / 0.3048" | bc`
-echo "$meter meter = $feet feet"
+case $choice in
+	$feetToInch)
+		read -p "Enter feet: " feet
+		inch=`echo "scale=4; $feet * 12" | bc`
+		echo "$feet feet = $inch inch";;
+	$inchToFeet)
+		read -p "Enter inch: " inch
+		feet=`echo "scale=4; $inch / 12" | bc`
+		echo "$inch inch = $feet feet";;
+	$feetToMeter)
+		read -p "Enter feet: " feet
+		meter=`echo "scale=4; $feet * 0.3048" | bc`
+		echo "$feet feet = $meter meter";;
+	$meterToFeet)
+		read -p "Enter meter: " meter
+		feet=`echo "scale=2; $meter / 0.3048" | bc`
+		echo "$meter meter = $feet feet";;
+esac
