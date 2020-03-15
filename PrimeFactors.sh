69626ff391d3f76624ed4270c0a38b265d82a2d1
balaji38738 <39882654+balaji38738@users.noreply.github.com>
2020-03-15 13:48:19 +0530

    Update PrimeFactors.sh

--- PrimeFactors.sh
+++ PrimeFactors.sh
@@ -12,9 +12,7 @@ for (( divisor=2; divisor<=$number; divisor++ ))
 do
 	while [ $(( number % divisor )) -eq 0 ]
 	do
-		primeFactors[((count++))]=$divisor
+		echo -n "$divisor"
 		number=$(( number / divisor ))
 	done
 done
-
-echo "The Prime factors are ${primeFactors[@]}"
